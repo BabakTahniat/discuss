a3ef326b790fd75bbf7fa007fcefeffdb66185ca
Babak <babak.tahniat@gmail.com>
2025-08-31 00:02:01 +0400

    implement server verification on server

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,10 +1,37 @@
 'use server';
-
 import db from '@/db';
+import { z } from 'zod';
 
-export async function createTopic(formData: FormData) {
-    const name = formData.get('name');
-    const description = formData.get('description');
+const TopicSchema = z.object({
+    name: z
+        .string()
+        .min(3, { error: 'Name must be at least 3 characters' })
+        .regex(/^[a-z-]+$/, {
+            error: 'Name must be lower case letters or dashes without spaces',
+        }),
+    description: z
+        .string()
+        .min(10, { error: 'Description must be at least 10 characters' }),
+});
+
+interface CreateTopicFomState {
+    name?: { errors: string[] };
+    description?: { errors: string[] };
+}
+
+export async function createTopic(
+    formState: CreateTopicFomState,
+    formData: FormData,
+): Promise<CreateTopicFomState> {
+    const data = Object.fromEntries(formData.entries());
+    const result = TopicSchema.safeParse(data);
+
+    if (!result.success) {
+        console.log(z.treeifyError(result.error).properties);
+        return z.treeifyError(result.error).properties as CreateTopicFomState;
+    }
+    return {};
+
     // await db.topic.create({ data: { description, slug } });
     //TODO: revalidate home page
 }
--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,11 +1,23 @@
+'use client';
 import { Button } from '@heroui/button';
 import { Popover, PopoverContent, PopoverTrigger } from '@heroui/popover';
 import { Input, Textarea } from '@heroui/input';
 import { createTopic } from '@/actions';
+import { startTransition, useActionState } from 'react';
 
 export default function TopicCreateForm() {
+    const [formState, action] = useActionState(createTopic, {});
+
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        startTransition(() => {
+            action(formData);
+        });
+    }
+
     return (
-        <Popover placement="left-start">
+        <Popover placement="left-start" className="dark">
             <PopoverTrigger>
                 <Button color="primary">Create a Topic</Button>
             </PopoverTrigger>
@@ -13,7 +25,8 @@ export default function TopicCreateForm() {
             <PopoverContent>
                 <form
                     className="flex flex-col gap-4 p-4 w-80"
-                    action={createTopic}
+                    onSubmit={handleSubmit}
+                    noValidate
                 >
                     <h3 className="text-lg">Create a topic</h3>
                     <Input
@@ -21,8 +34,9 @@ export default function TopicCreateForm() {
                         labelPlacement="outside"
                         placeholder="Name"
                         name="name"
+                        isInvalid={!!formState.name}
+                        errorMessage={formState.name?.errors.join('.\n')}
                     />
-
                     <Textarea
                         label="description"
                         labelPlacement="outside"
